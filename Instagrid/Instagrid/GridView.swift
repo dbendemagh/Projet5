9f78a19c2f9f18a9f84974bbd7082da96cc710c4
Daniel Bendemagh <daniel.bendemagh@laposte.net>
2018-05-09 00:37:16 +0200

    Add images layout and buttons

---
+++ Instagrid/Instagrid/GridView.swift
@@ -0,0 +1,21 @@
+//
+//  GridView.swift
+//  Instagrid
+//
+//  Created by Daniel BENDEMAGH on 09/05/2018.
+//  Copyright © 2018 Daniel BENDEMAGH. All rights reserved.
+//
+
+import UIKit
+
+class GridView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
