5bea63811b1d2b4dbb0d737be3a5114459d37cd8
Daniel Bendemagh <daniel.bendemagh@laposte.net>
2018-05-13 17:59:52 +0200

    Add Stack Views and icons for buttons

--- Instagrid/Instagrid/GridView.swift
+++ Instagrid/Instagrid/GridView.swift
@@ -10,12 +10,10 @@ import UIKit
 
 class GridView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    @IBOutlet weak var exempleView: UIView!
+    
+    func displayPattern1() {
+        exempleView.isHidden = true
     }
-    */
-
 }
