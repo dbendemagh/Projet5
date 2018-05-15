607ac3df06d91e8e5e8db9d6844d70c95c3e9e86
Daniel Bendemagh <daniel.bendemagh@laposte.net>
2018-05-16 00:15:55 +0200

    Add MVC folders

--- Instagrid/Instagrid/Controllers/ViewController.swift
+++ Instagrid/Instagrid/Controllers/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var gridView: GridView!
+
+    @IBOutlet var layoutButtons: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,8 +21,9 @@ class ViewController: UIViewController {
         
     }
 
-    
-
-
 }
 
+extension ViewController {
+    
+    
+}
